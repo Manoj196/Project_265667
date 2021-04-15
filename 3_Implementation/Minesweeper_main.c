32dca1abfd0cc219ddac815124ed524bacb322cb
Manoj196 <56036534+Manoj196@users.noreply.github.com>
2021-04-15 12:11:39 +0530

    Update Minesweeper_main.c

--- 3_Implementation/Minesweeper_main.c
+++ 3_Implementation/Minesweeper_main.c
@@ -1,39 +1,41 @@
-#include<Minesweeper_operations.h>>
-char grid[10][10], key[10][10];
-int mine[10];
-int px = 0, py = 0;
-int win = 0, lose = 0;
-int firstClick = 0;
+#include "Minesweeper_operations.c"
 
-int main ()
+
+
+int main()
 {
   char c;
-  init ();
-clrscr();
-  display ();
+  init();
+
+  system("cls");
+  display();
+
   do
   {
-    c = getch ();
-    if (c == 'w')
-    moveCursor (-1); // moves cursor up
-    else if (c == 's')
-    moveCursor (1); // moves cursor down
-    else if (c == 'd')
-    moveCursor (10); // moves cursor right
-    else if (c == 'a')
-    moveCursor (-10); // moves cursor left
-    else if (c == ' ')
+    c = getch();
+    if(c == 'w')
+    moveCursor(-1); // moves cursor up
+    else if(c == 's')
+    moveCursor(1); // moves cursor down
+    else if(c == 'd')
+    moveCursor(10); // moves cursor right
+    else if(c == 'a')
+    moveCursor(-10); // moves cursor left
+    else if(c == ' ')
     click(px,py);
-    if (c == 'f')
-    flag ();
-   clrscr();
-    display ();
-    if (lose == 1)
-    printf ("YOU LOST!!!");
-    if (win == 90)
-    printf ("YOU WIN!!!!");
-  }while (c!='e' && lose == 0 && win != 90);
-  getch ();
-  clrscr();
+    if(c == 'f')
+    flag();
+
+    system("cls");
+    display();
+    if(lose == 1)
+    printf("YOU LOST!!!");
+    if(win == 90)
+    printf("YOU WIN!!!!");
+  }while(c!='e' && lose == 0 && win != 90);
+
+  getch();
+  system("cls");
+
   return 0;
 }
