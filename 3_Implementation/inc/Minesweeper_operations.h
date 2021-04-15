9aabb3856791680972a192fea1b211a2a01071df
Manoj196 <56036534+Manoj196@users.noreply.github.com>
2021-04-15 12:07:17 +0530

    Update Minesweeper_operations.h

--- 3_Implementation/inc/Minesweeper_operations.h
+++ 3_Implementation/inc/Minesweeper_operations.h
@@ -1,23 +1,65 @@
 /**
  * @file Minesweeper_operations.h
  * @author Manoj R (you@domain.com)
- * @brief 
+ * @brief Header files
  * @version 0.1
- * @date 2021-04-14
+ * @date 2021-04-15
  * 
  * @copyright Copyright (c) 2021
  * 
  */
- 
+
+
+
+#ifndef __MINESWEEPER_OPERATIONS_H__
+#define __MINESWEEPER_OPERATIONS_H__
+
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-//#include <conio.h>
-#include <termios.h>
-#include <unistd.h>
+#include <conio.h>
+// #include <termios.h>
+ //#include <unistd.h>
 
-char grid[10][10], key[10][10];
-int mine[10];
-int px = 0, py = 0;
-int win = 0, lose = 0;
-int firstClick = 0;
+/**
+ * @brief // initializes all grid tiles to '.'
+ * 
+ */
+void init();
+/**
+ * @brief //prints grid with the cursor represented by <>
+ * 
+ */
+void display();
+/**
+ * @brief to generate mines
+ * 
+ */
+void genMine();
+/**
+ * @brief to generate keys
+ * 
+ */
+void keygen();
+/**
+ * @brief to flag a probable mine point
+ * 
+ */
+void flag();
+/**
+ * @brief 
+ * 
+ * @param n is to move on points
+ */
+void moveCursor(int n);
+/**
+ * @brief 
+ * 
+ * @param x points
+ * @param y points
+ */
+void click(int x, int y);
+int main();
+#endif
